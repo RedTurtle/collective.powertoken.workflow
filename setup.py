fc15456d2050719dce4a0434e0846f44a29fcbc4
Luca Fabbri <luca@keul.it>
2012-01-10 18:24:36 +0100

    Updated egg info for Plone 4 compatibility

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 import os
 
+tests_require = ['zope.testing', 'Products.PloneTestCase']
+
 version = '0.1.0'
 
 setup(name='collective.powertoken.workflow',
@@ -13,6 +15,10 @@ setup(name='collective.powertoken.workflow',
       classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
@@ -30,6 +36,8 @@ setup(name='collective.powertoken.workflow',
           'setuptools',
           'collective.powertoken.core',
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
       entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
